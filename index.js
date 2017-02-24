c51867c1c01be94a77d88a0c9a89ead74e5d4f6f
Jake Sparling <jsparling@avvo.com>
2017-02-23 16:31:27 -0800

    cleaner clean up

--- index.js
+++ index.js
@@ -3,13 +3,11 @@
 var bookshelf = require('./bookshelf')
 var Article = require("./models/article")
 
-var getInsertedArticle = (id) =>{
+var getInsertedArticle = (id, callback) =>{
   console.log("\nnow get the article from the db\n")
   Article.where('id', id).fetch().then(function(article) {
     console.log(article)
-
-    //destroy the database connection
-    bookshelf.knex.destroy()
+    callback(article)
   })
 }
 
@@ -24,13 +22,24 @@ var insertArticle = (callback) =>{
     var insertedId = saved.attributes.id
 
     callback(insertedId)
+    // bookshelf.knex.destroy()
 
     //destroy the database connection
-    // bookshelf.knex.destroy()
+    //
   })
 }
 
 // insert the article, and when we are done, get the inserted article
 // insertArticle(function(id){ console.log("inserted article with id: " + id) })
 
-insertArticle(getInsertedArticle)
+insertArticle(function(id) {
+  getInsertedArticle(id, function(article) {
+    bookshelf.knex.destroy()
+
+    bookshelf = require('./bookshelf')
+    insertArticle(function(id) {
+      console.log(id);
+
+    })
+  })
+})
